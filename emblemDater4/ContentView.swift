52a128dfff7eb94828f9d22f84796509a60e6735
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2020-08-02 00:15:06 -0500

    Initial Commit

---
+++ emblemDater4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  emblemDater4
+//
+//  Created by Ricardo Herrera Petit on 8/2/20.
+//  Copyright © 2020 Ricardo Herrera Petit. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
